6e734d341e7e1f40d54d40c3c3685693aebb086e
meandrewaprianto <andrewaprianto@AndrewApriantos-MacBook-Pro.local>
2023-07-21 10:39:58 +0700

    Pembuatan Tabel

---
+++ database/migrations/2023_07_21_022935_buat_tabel_member.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('member', function(Blueprint $table){
+            $table->increments('id_member');
+            $table->bigInteger('kode_member')->unsigned();
+            $table->string('nama', 100);
+            $table->text('alamat');
+            $table->string('telpon', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('member');
+    }
+};
---
+++ database/migrations/2023_07_21_030616_buat_tabel_penjualan_detail.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('penjualan_detail', function(Blueprint $table){
+            $table->increments('id_penjualan_detail');
+            $table->integer('id_penjualan')->unsigned();
+            $table->bigInteger('kode_produk')->unsigned();
+            $table->bigInteger('harga_jual')->unsigned();
+            $table->integer('jumlah')->unsigned();
+            $table->integer('diskon')->unsigned();
+            $table->bigInteger('sub_total')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('penjualan_detail');
+    }
+};
