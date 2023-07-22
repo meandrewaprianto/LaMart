d31caeeff2bedb2eac40ced8958e5218a77662bd
meandrewaprianto <andrewaprianto@AndrewApriantos-MacBook-Pro.local>
2023-07-22 09:19:14 +0700

    PenyempurnaanModul_login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,77 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <title>LaMart | Log in</title>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+        <!-- Google Font: Source Sans Pro -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="{{ asset('adminLTE/plugins/fontawesome-free/css/all.min.css') }}"/>
+
+        <!-- Theme style -->
+        <link rel="stylesheet" href="{{ asset('adminLTE/dist/css/adminlte.min.css') }}"/>
+
+        <style>
+            /* Remove default border, added new one */
+            .form-control {
+                border-right: 1px solid #ccc !important;
+            }
+        </style>
+    </head>
+    <body class="hold-transition login-page">
+        <!-- login-box -->
+        <div class="login-box">
+            <div class="login-logo">
+                <img src="{{ asset('images/logo.png') }}" alt="Logo LaMart" width="150" />
+            </div>
+            
+            <!-- card -->
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-body login-card-body">
+                    <p class="login-box-msg">Login untuk menggunakan aplikasi</p>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+                    <form action="{{ route('login') }}" method="post">
+                        {{ csrf_field() }}
+                        <div class="input-group mb-3">
+                            <input type="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" name="email" value="{{ old('email') }}" required autofocus />
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-user"></span>
                                 </div>
                             </div>
                         </div>
 
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
+                        <div class="input-group mb-3">
+                            <input type="password" class="form-control" placeholder="Password" name="password" required />
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-cog"></span>
+                                </div>
                             </div>
+                        </div>
+
+                        <div class="col-4">
+                            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
                         </div>
                     </form>
                 </div>
             </div>
+            <!-- ./card -->
         </div>
-    </div>
-</div>
-@endsection
+        <!-- ./login-box -->
+        
+        <!-- jQuery -->
+        <script src="{{ asset('adminLTE/plugins/jquery/jquery.min.js') }}"></script>
+        <!-- jquery-validation -->
+        <script src="{{ asset('adminLTE/plugins/jquery-validation/jquery.validate.min.js') }}"></script>
+        <script src="{{ asset('adminLTE/plugins/jquery-validation/additional-methods.min.js') }}"></script>
+        <!-- Bootstrap 4 -->
+        <script src="{{ asset('adminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+        <!-- AdminLTE App -->
+        <script src="{{ asset('adminLTE/dist/js/adminlte.min.js') }}"></script>
+
+    </body>
+</html>
